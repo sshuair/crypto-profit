1536837b0c500ff3cc0ea7a73ce7db8b1bc21757
sshuair <sshuair@gmail.com>
2021-05-05 21:32:05 +0800

    print with dollor

--- crypto_profit/profit.py
+++ crypto_profit/profit.py
@@ -75,7 +75,7 @@ def main():
             roi_result.append(roi_symbol)
     roi_result = sorted(roi_result, key=lambda k: k['holding_profit_percent'], reverse=True) 
     summary_table = PrettyTable()
-    summary_table.field_names = ['名称','持仓成本价','当前价格','持仓数量','持仓成本总价','当前总价','持仓收益','持仓收益率']
+    summary_table.field_names = ['名称','持仓成本价($)','当前价格($)','持仓数量','持仓成本总价($)','当前总价($)','持仓收益($)','持仓收益率(%)']
     for item in roi_result:
         summary_table.add_row([item['name'], 
                                 item['holding_avg_price'], item['current_price'], 
